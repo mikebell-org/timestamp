6a5eeeebf9d8c1b1eafd2f9304546cf1c858ecc1
Mike Bell <mike@mikebell.org>
2015-03-04 15:19:19 -0800

    Initial code checkin

---
+++ main.go
@@ -0,0 +1,21 @@
+package timestamp
+
+import "time"
+
+type Timestamp float64
+
+const nano = 1000000000.0
+
+func New(t time.Time) Timestamp {
+	return Timestamp(Timestamp(t.UnixNano()) / nano)
+}
+
+func (t Timestamp) Time() time.Time {
+	sec := int64(t)
+	nsec := int64((t - Timestamp(sec)) * nano)
+	return time.Unix(sec, nsec)
+}
+
+func (t Timestamp) String() string {
+	return t.Time().String()
+}
